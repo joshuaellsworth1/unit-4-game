c02dad282ec0a28d974057fe8cf40af69aaeed2e
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-07-03 15:09:12 -0400

    Reset game when user wins or loses @add images to page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,51 @@
+
+var win = 0;
+var loss = 0;
+var counter = 0;
+var boss = 0;
+
+function resetGame() {
+    counter = 0;
+    randomNum1 = 0;
+    randomNum2 = 0;
+    randomNum3 = 0;
+    randomNum4 = 0;
+    boss = 0;
+
+    randomNum1 = Math.floor(Math.random() * 12 + 1);
+    randomNum2 = Math.floor(Math.random() * 12 + 1);
+    randomNum3 = Math.floor(Math.random() * 12 + 1);
+    randomNum4 = Math.floor(Math.random() * 12 + 1);
+    boss = Math.floor(Math.random() * 120 + 19);
+
+    $("#number").text(boss);
+    $("#group-attack").text(counter);
+
+
+}
+
+function gameCondition() {
+    if (counter === boss) {
+        win++;
+        $("#win").text(win);
+        resetGame();
+    }
+
+    else if (counter >= boss) {
+        loss++;
+        $("#loss").text(loss);
+        resetGame();
+    }
+}
+
+
 $(document).ready(function () {
-    var boss = Math.floor(Math.random() * 100 + 20);
+    
+    boss = Math.floor(Math.random() * 120 + 19);
 
     $("#number").text(boss);
 
-    var win = 0;
-    var loss = 0;
 
-    var imageB = $("#bowser");
-
-    var counter = 0;
     $("#group-attack").text(counter);
     var image1 = $("#kong");
     var image2 = $("#kirby");
@@ -29,18 +66,8 @@ $(document).ready(function () {
         var currentValue = parseInt(($(this).attr("group-attack")));
         counter += currentValue;
         $("#group-attack").text(counter);
+        gameCondition()
 
-        if (counter === boss) {
-            win++;
-            $("#win").text(win);
-            return
-        }
-
-        else if (counter >= boss) {
-            loss++;
-            $("#loss").text(loss);
-            return
-        }
     })
 
 });
